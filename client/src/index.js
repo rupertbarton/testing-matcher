e8ac8eef3d356aaab134d0ef388d9e76b74bc1c4
Elliott Reid <ereid@scottlogic.com>
2021-08-13 09:29:53 +0100

    Built a calculator

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
+function Screen(props) {
+  return <button className="screen">{props.output}</button>;
+}
+
 function Square(props) {
   return (
     <button className="square" onClick={props.onClick}>
@@ -11,37 +15,41 @@ function Square(props) {
 }
 
 class Board extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { squares: Array(9).fill(null) };
+  renderScreen(output) {
+    return <Screen output={output} />;
   }
 
-  renderSquare(i) {
-    return (
-      <Square
-        value={this.props.squares[i]}
-        onClick={() => this.props.onClick(i)}
-      />
-    );
+  renderSquare(i, onClick = this.onClick) {
+    return <Square value={i} onClick={() => this.props.onClick(i)} />;
   }
 
   render() {
     return (
       <div>
+        <div className="screen">{this.renderScreen(this.props.output)}</div>
         <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
           {this.renderSquare(7)}
           {this.renderSquare(8)}
+          {this.renderSquare(9)}
+          {this.renderSquare("/")}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+          {this.renderSquare(6)}
+          {this.renderSquare("*")}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+          {this.renderSquare(3)}
+          {this.renderSquare("+")}
+        </div>
+        <div className="board-row">
+          {this.renderSquare("C")}
+          {this.renderSquare(0)}
+          {this.renderSquare("=")}
+          {this.renderSquare("-")}
         </div>
       </div>
     );
@@ -52,93 +60,79 @@ class Game extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      history: [
-        {
-          squares: Array(9).fill(null),
-        },
-      ],
-      stepNumber: 0,
-      xIsNext: true,
+      output: [],
+      sum: "",
     };
   }
 
   handleClick(i) {
-    const history = this.state.history.slice(0, this.state.stepNumber + 1);
-    const current = history[history.length - 1];
-    const squares = current.squares.slice();
-    if (calculateWinner(squares) || squares[i]) {
-      return;
+    const operations = ["", "C", "=", "+", "-", "/", "*"];
+    let currentOutput = this.state.output;
+    if (i === "C") {
+      this.setState({ output: [], sum: "" });
+    } else if (i === "=") {
+      let sum = calculateValue(this.state.output);
+      this.setState({ output: [sum], sum: sum });
+    } else if (operations.find((item) => item === i) !== undefined) {
+      currentOutput = currentOutput.concat([i]);
+      this.setState({ output: currentOutput, sum: currentOutput.join("") });
+    } else {
+      let previous = currentOutput[currentOutput.length - 1] || "";
+      if (operations.find((item) => item === previous) === undefined) {
+        currentOutput[currentOutput.length - 1] += i.toString();
+      } else {
+        currentOutput.push(i.toString());
+      }
+      this.setState({ output: currentOutput, sum: currentOutput.join("") });
     }
-    squares[i] = this.state.xIsNext ? "X" : "O";
-    this.setState({
-      history: history.concat([{ squares: squares }]),
-      stepNumber: history.length,
-      xIsNext: !this.state.xIsNext,
-    });
-  }
-
-  jumpTo(step) {
-    this.setState({
-      stepNumber: step,
-      xIsNext: step % 2 === 0,
-    });
   }
 
   render() {
-    const history = this.state.history;
-    const current = history[this.state.stepNumber];
-    const winner = calculateWinner(current.squares);
-
-    const moves = history.map((step, move) => {
-      const desc = move ? "Go to move #" + move : "Go to game start";
-      return (
-        <li key={move}>
-          <button onClick={() => this.jumpTo(move)}> {desc} </button>
-        </li>
-      );
-    });
-
-    let status;
-    if (winner) {
-      status = "Winner:" + winner;
-    } else {
-      status = "Next player: " + (this.state.xIsNext ? "X" : "O");
-    }
     return (
       <div className="game">
         <div className="game-board">
           <Board
-            squares={current.squares}
+            output={this.state.output}
             onClick={(i) => this.handleClick(i)}
           />
         </div>
         <div className="game-info">
-          <div>{status}</div>
-          <ol>{moves}</ol>
+          <div>{}</div>
+          <ol>{}</ol>
         </div>
       </div>
     );
   }
 }
 
-function calculateWinner(squares) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
+function calculateValue(output) {
+  try {
+    for (let i = 1; i < output.length; i++) {
+      if (output[i] === "*") {
+        output[i - 1] = Number(output[i - 1]) * Number(output[i + 1]);
+        output.splice(i, 2);
+        i--;
+      } else if (output[i] === "/") {
+        output[i - 1] = Number(output[i - 1]) / Number(output[i + 1]);
+        output.splice(i, 2);
+        i--;
+      }
     }
+    for (let i = 1; i < output.length; i++) {
+      if (output[i] === "+") {
+        output[i - 1] = Number(output[i - 1]) + Number(output[i + 1]);
+        output.splice(i, 2);
+        i--;
+      } else if (output[i] === "-") {
+        output[i - 1] = Number(output[i - 1]) - Number(output[i + 1]);
+        output.splice(i, 2);
+        i--;
+      }
+    }
+    return output[0];
+  } catch (err) {
+    return "Err";
   }
-  return null;
 }
 
 // ========================================
