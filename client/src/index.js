612ba069732a17ea827d6f5febe41488ab513f6c
Elliott Reid <ereid@scottlogic.com>
2021-08-13 10:01:09 +0100

    Converted Board to function

--- client/src/index.js
+++ client/src/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
@@ -14,41 +14,41 @@ function Square(props) {
   );
 }
 
+function renderScreen(output) {
+  return <Screen output={output} />;
+}
+
+function renderSquare(i, props) {
+  return <Square value={i} onClick={() => props.onClick(i)} />;
+}
+
 function Board(props) {
-  this.renderScreen = function (output) {
-    return <Screen output={output} />;
-  };
-
-  this.renderSquare = function (i, onClick = this.onClick) {
-    return <Square value={i} onClick={() => this.props.onClick(i)} />;
-  };
-
   return (
     <div>
-      <div className="screen">{this.renderScreen(this.props.output)}</div>
+      <div className="screen">{renderScreen(props.output)}</div>
       <div className="board-row">
-        {this.renderSquare(7)}
-        {this.renderSquare(8)}
-        {this.renderSquare(9)}
-        {this.renderSquare("/")}
+        {renderSquare(7, props)}
+        {renderSquare(8, props)}
+        {renderSquare(9, props)}
+        {renderSquare("/", props)}
       </div>
       <div className="board-row">
-        {this.renderSquare(4)}
-        {this.renderSquare(5)}
-        {this.renderSquare(6)}
-        {this.renderSquare("*")}
+        {renderSquare(4, props)}
+        {renderSquare(5, props)}
+        {renderSquare(6, props)}
+        {renderSquare("*", props)}
       </div>
       <div className="board-row">
-        {this.renderSquare(1)}
-        {this.renderSquare(2)}
-        {this.renderSquare(3)}
-        {this.renderSquare("+")}
+        {renderSquare(1, props)}
+        {renderSquare(2, props)}
+        {renderSquare(3, props)}
+        {renderSquare("+", props)}
       </div>
       <div className="board-row">
-        {this.renderSquare("C")}
-        {this.renderSquare(0)}
-        {this.renderSquare("=")}
-        {this.renderSquare("-")}
+        {renderSquare("C", props)}
+        {renderSquare(0, props)}
+        {renderSquare("=", props)}
+        {renderSquare("-", props)}
       </div>
     </div>
   );
